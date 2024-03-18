b5aaa98f77e95fd001c1fb27f532003cf00b89e7
Chetan kaushik <chetankaushik29@gmail.com>
2024-03-18 19:13:45 +0530

    physical fitness web is created successfully

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Screen/Screen'; // Assuming 'Home' is the correct component name
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
